df70534d82d2fd37b9b42be4175e5df7688c0a54
kimuit117 <kemoikiuita@gmail.com>
2022-10-25 09:51:04 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,24 @@
-class Dog
-  def bark
-    puts "Woof!"
-  end
+# class Dog
+#   def bark
+#     puts "Woof!"
+#   end
 
-  def sit
-      puts "The Dog is sitting"
-  end
+#   def sit
+#       puts "The Dog is sitting"
+#   end
+# end
+
+# fido = Dog.new
+# fido.bark
+# # start woofing
+
+class Dog
+
+def bark
+puts "Woof!"
+end
+def sit
+puts "The Dog is sitting"
 end
 
-fido = Dog.new
-fido.bark
-# start woofing
+end
